798435f96d3dc3255a18faab07e0253cd172a26d
Ugur-Ergun <ugur.ergun@outlook.com>
2022-10-21 21:29:06 +0300

    Init

---
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using ECommerceAPI.Application.Abstranction;
+using ECommerceAPI.Persistance.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}

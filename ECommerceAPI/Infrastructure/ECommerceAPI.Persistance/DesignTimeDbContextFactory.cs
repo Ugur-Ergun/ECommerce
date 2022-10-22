968e501a2fe5e2744db91036bc8322441713c635
Ugur-Ergun <ugur.ergun@outlook.com>
2022-10-22 12:43:20 +0300

    Entities and migration implementation

---
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistance/DesignTimeDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using ECommerceAPI.Persistance.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistance
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ECommerceAPIDbContext>
+    {
+        //For dotnet CLI create migration. It will fill dbContext contstruction option parameter
+        public ECommerceAPIDbContext CreateDbContext(string[] args)
+        {
+            //getting configuration from Json file without build
+            
+
+            DbContextOptionsBuilder<ECommerceAPIDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseNpgsql(Configuration.ConnectionString);
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
---
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using ECommerceAPI.Persistance.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ECommerceAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+        }
+    }
+}
